ce596e829c04a8110096996adcf0a9e0b74fd1d3
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2026-02-26 20:55:07 +0100

    remove Interactiontest

--- Content.IntegrationTests/Tests/Chemistry/ReagentDataTest.cs
+++ Content.IntegrationTests/Tests/Chemistry/ReagentDataTest.cs
@@ -1,4 +1,3 @@
-using Content.IntegrationTests.Tests.Interaction;
 using Content.Shared.Chemistry.Reagent;
 using Robust.Shared.Reflection;
 using Robust.Shared.Serialization;
@@ -6,9 +5,8 @@ using Robust.Shared.Utility;
 
 namespace Content.IntegrationTests.Tests.Chemistry;
 
-[TestFixture]
 [TestOf(typeof(ReagentData))]
-public sealed class ReagentDataTest : InteractionTest
+public sealed class ReagentDataTest
 {
     [Test]
     public async Task ReagentDataIsSerializable()
